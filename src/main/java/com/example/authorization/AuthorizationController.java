31b5665cf4d8efd17afb7b1afb813493decde1d3
IvanKustarev <ivan.kustarev@gmail.com>
2021-11-01 10:15:10 +0300

    finish 0.1

--- src/main/java/com/example/authorization/AuthorizationController.java
+++ src/main/java/com/example/authorization/AuthorizationController.java
@@ -14,56 +14,66 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Iterator;
 
 @Controller
-@RequestMapping("/")
 public class AuthorizationController {
     @Autowired
     private UsersRepository usersRepository;
 
     @GetMapping("")
     public String login() {
-        return "redirect:/sign";
+        return "redirect:/authorisation/sign";
     }
 
-    @GetMapping("/registration")
+    @GetMapping("/authorisation/")
+    public String authorisation() {
+        return "redirect:/authorisation/sign";
+    }
+
+    @GetMapping("/authorisation/registration")
     public String registrationGet() {
         return "registration";
     }
 
-    @PostMapping("/registration")
-    public String registrationPost(@RequestParam String login, @RequestParam String password, Model model) {
+    @PostMapping("/authorisation/registration")
+    public String registrationPost(@RequestParam String login, @RequestParam String password, @RequestParam("fullName") String fullName, @RequestParam("address") String address, Model model) {
         Users user = findUserByLogin(login);
         if (user != null) {
             model.addAttribute("message", "Пользователь с таким именем уже есть!");
-            return "registration";
+            model.addAttribute("toMainURL", "/authorisation/registration");
+            return "message";
         } else {
             //успешно зарегестрировался
             user = new Users(login, password);
+            user.setFullName(fullName);
+            user.setAddress(address);
+            user.setBalance(new Long(0));
             usersRepository.save(user);
             return "redirect:http://localhost:8082/profile/" + user.getId() + "/";
         }
     }
 
-    @GetMapping("/sign")
+    @GetMapping("/authorisation/sign")
     public String signGet() {
         return "sign";
     }
 
-    @PostMapping("/sign")
+    @PostMapping("/authorisation/sign")
     public String signPost(@RequestParam String login, @RequestParam String password, Model model) {
 
         Users user = findUserByLogin(login);
 
         if (user == null) {
             model.addAttribute("message", "Пользователя с таким именем не существует!");
-            return "registration";
+            model.addAttribute("toMainURL", "/authorisation/sign");
+            return "message";
         } else  {
             if (user.getPassword().equals(password)) {
                 //успешно вошёл
                 return "redirect:http://localhost:8082/profile/" + user.getId() + "/";
             } else {
                 model.addAttribute("message", "Неверный пароль!");
+                model.addAttribute("toMainURL", "/authorisation/sign");
+                return "message";
             }
-            return "sign";
         }
     }
 
